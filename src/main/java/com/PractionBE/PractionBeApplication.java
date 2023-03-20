e19da04fccd399ed57819a0e69dbd5c8c9c8df9f
Airvw <airvw14@gmail.com>
2023-03-20 20:50:53 +0900

    개발환경 세팅

---
+++ src/main/java/com/PractionBE/PractionBeApplication.java
@@ -0,0 +1,13 @@
+package com.PractionBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractionBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractionBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PractionBE/PractionBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PractionBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractionBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
